e24740e2252f0f2318bb266b3926892e86f32e05
Paulo Weskley A. Ferreira <paulo.weskley@pontomais.com.br>
2024-01-08 09:27:34 -0300

    feature: creation of the unit test file for base class

--- src/app/vgr-fields/vrg-field-number/vrg-field-number.component.spec.ts
+++ src/app/vgr-fields/vrg-field-number/vrg-field-number.component.spec.ts
@@ -1,25 +1,47 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { ReactiveFormsModule } from '@angular/forms'
 
-import { VrgFieldNumberComponent } from './vrg-field-number.component';
+import { VrgFieldNumberComponent } from './vrg-field-number.component'
 
 describe(`${VrgFieldNumberComponent.name}`, () => {
-  let component: VrgFieldNumberComponent;
-  let fixture: ComponentFixture<VrgFieldNumberComponent>;
+  let component: VrgFieldNumberComponent
+  let fixture: ComponentFixture<VrgFieldNumberComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
       declarations: [ VrgFieldNumberComponent ]
     })
-    .compileComponents();
-  }));
+    .compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(VrgFieldNumberComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(VrgFieldNumberComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+
+  it('should have default value of 0', () => {
+    expect(component.value).toBe(0)
+  })
+
+  it('should have default step of undefined', () => {
+    expect(component.step).toBeUndefined()
+  })
+
+  it('should initialize with checkPlaceholder', () => {
+    spyOn(component, 'checkPlaceholder')
+    component.ngOnInit()
+    expect(component.checkPlaceholder).toHaveBeenCalled()
+  })
+
+  it('should set placeholder to empty string in checkPlaceholder if not provided', () => {
+    component.placeholder = undefined
+    component.checkPlaceholder()
+    expect(component.placeholder).toEqual('')
+  })
+})
